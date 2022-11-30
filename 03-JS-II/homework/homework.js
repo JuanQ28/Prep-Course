d59864b6a8cea7c531701558b367e4cc8a598d52
JuanQ <elquinteje@hotmail.com>
2022-11-29 20:59:43 -0500

    primer modificacion

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,7 +5,14 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if(x>y)
+  {return x;}
+  if(y>x)
+  {return y;}
+  if(x===y)
+  {return x;}
 }
+
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
